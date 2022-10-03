9afdef57e7456d68f0dfb9f3d51143894664b7e6
David Ricketts <david.ricketts119@gmail.com>
2022-10-03 05:38:34 -0400

    Week 1 Workshop

---
+++ routes/promotionRouter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const promotionRouter = express.Router();
+
+promotionRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+.post((req, res) => {
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+});
+
+
+promotionRouter.route('/:promotionId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`'Will send promotion number ${req.params.promotionId} to you'`);
+})
+.post((req, res) => {
+    // not allowed
+    res.statusCode = 403;
+    res.end(`operation not supported on promotion ${req.params.promotionId}`);
+})
+.put((req, res) => {
+    // will be allowed
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting promotion ${req.params.promotionId}`);
+});
+module.exports = promotionRouter;
